d37f44c8b2bade885c3180f8731baa339ed64175
github greenmmmm <green_apple_chair_card392@yahoo.co.jp>
2021-03-07 16:13:03 +0900

    getter setterを追加

--- src/main/java/com/example/demo/domain/Okashi.java
+++ src/main/java/com/example/demo/domain/Okashi.java
@@ -2,4 +2,14 @@ package com.example.demo.domain;
 
 public class Okashi {
 
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 }
