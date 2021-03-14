2f351dccb4b32aea1587dafdeaabbd89ed174461
github greenmmmm <green_apple_chair_card392@yahoo.co.jp>
2021-03-14 14:47:39 +0900

    idとカロリーの表示

--- src/main/java/com/example/demo/domain/Okashi.java
+++ src/main/java/com/example/demo/domain/Okashi.java
@@ -1,8 +1,25 @@
 package com.example.demo.domain;
 
 public class Okashi {
-
+	private String id;
 	private String name;
+	private String kal;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getKal() {
+		return kal;
+	}
+
+	public void setKal(String kal) {
+		this.kal = kal;
+	}
 
 	public String getName() {
 		return name;
