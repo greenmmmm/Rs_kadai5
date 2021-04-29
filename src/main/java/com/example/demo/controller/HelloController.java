34a2fc51f5744a9c6cb305f708b9b20918d24eb0
github greenmmmm <green_apple_chair_card392@yahoo.co.jp>
2021-04-29 14:55:46 +0900

    登録画面に接続

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -31,5 +33,11 @@ public class HelloController {
     	model.addAttribute("okashi", list);
 
         return "index";
+    }
+    
+    @GetMapping("/register")
+    public String registerUser(Model model, @ModelAttribute Okashi o) {
+        model.addAttribute("okashi", o);
+        return "/";
     }
 }
