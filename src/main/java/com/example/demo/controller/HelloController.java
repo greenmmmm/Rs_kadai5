dd714bc34d33ac01a4ee27556a7708fbcc10b964
github greenmmmm <green_apple_chair_card392@yahoo.co.jp>
2021-04-30 14:38:11 +0900

    変更画面に移動

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -47,5 +48,12 @@ public class HelloController {
     public String create(@ModelAttribute Okashi o) {
         service.insertOne(o);
         return "redirect:/";
+    }
+
+    //変更ボタンを押すと、変更画面に移動
+    @GetMapping("change/id={id}")
+    public String change(@PathVariable("id") String id, Model model) {
+        model.addAttribute("okashi", service.getOne(id));
+        return "change";
     }
 }
